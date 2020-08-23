b8ab5afcf203caa49bb7ab1ee0b568a2c9a41380
Jhossept Garay <jhrodriguez@externos.technisys.com>
2020-08-23 03:49:27 -0500

    prepared to deploy

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
-
+const path = require('path');
 const app = express();
 
 //Connect DB
@@ -8,8 +8,6 @@ connectDB();
 
 //Init Middleware
 app.use(express.json({ extended: false }));
-
-app.get('/', (req, res) => res.send('API Running'));
 
 //Define routes
 app.use('/api/users', require('./routes/api/users'));
@@ -19,6 +17,17 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/events', require('./routes/api/events'));
 app.use('/api/riders', require('./routes/api/riders'));
 app.use('/api/votes', require('./routes/api/votes'));
+
+//Serve static assets in production
+if (proccess.env.NODE_ENV === 'production') {
+  //Set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path, resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
