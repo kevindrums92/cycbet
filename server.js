6933347dc0c769a180c35f079706789d42af0523
Jhossept Garay <jhrodriguez@externos.technisys.com>
2020-08-24 22:10:49 -0500

    fix internal server error in routing heroku

--- server.js
+++ server.js
@@ -25,7 +25,10 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 
   app.get('*', (req, res) => {
-    res.sendFile(path, resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(
+      path,
+      path.resolve(__dirname, 'client', 'build', 'index.html')
+    );
   });
 }
 
