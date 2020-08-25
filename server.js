10ebcde19a6b09877ad6a0bcdf2840e4746b736d
Jhossept Garay <jhrodriguez@externos.technisys.com>
2020-08-24 23:05:12 -0500

    env string connection

--- server.js
+++ server.js
@@ -24,12 +24,6 @@ if (process.env.NODE_ENV === 'production') {
   //Set static folder
   app.use(express.static('client/build'));
 
-  // app.get('*', (req, res) => {
-  //   res.sendFile(
-  //     path,
-  //     path.resolve(__dirname, 'client', 'build', 'index.html')
-  //   );
-  // });
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname + '/client/build/index.html'));
   });
